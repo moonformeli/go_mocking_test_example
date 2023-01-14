b6a44062c92b68e6a1bbac5cf959160d6f3cac33
moonformeli <mgyang95@gmail.com>
2023-01-14 16:58:27 +0900

    :sparkles: Finish to write usecase

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/moonformeli/go_mocking_test_example
+
+go 1.19
--- internal/usecase/user.go
+++ internal/usecase/user.go
@@ -1,6 +1,9 @@
 package usecase
 
+import entity "github.com/moonformeli/go_mocking_test_example/internal/entity"
+
 /** In this interface, we can write features we need to implement */
 type User interface {
-	GetUserByName(name string) User
+	GetUserByName(name string) (entity.User, error)
+	CountUser() (int, error)
 }
