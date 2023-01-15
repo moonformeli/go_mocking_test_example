4b5597a751d1cb6a584f012a93e0d3c7678c8f86
moonformeli <mgyang95@gmail.com>
2023-01-15 18:37:56 +0900

    :sparkles: Create a db instance

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/moonformeli/go_mocking_test_example/internal/db"
+)
 
 func main() {
 	log.Println("main.go")
+
+	db := db.GetDatabase()
+	log.Println(db)
 }
